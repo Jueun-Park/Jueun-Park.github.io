1d3448cf88062c79d4f99fd60fd5e0a9fbe87ab9
Jueun <elpion19@gmail.com>
2018-10-05 13:50:31 +0900

    change InfiniteScroll.js

--- assets/lib/InfiniteScroll.js
+++ assets/lib/InfiniteScroll.js
@@ -1,6 +1,14 @@
-class InfiniteScroll {
-    constructor (path, wrapperId) {
-        if (path === undefined || wrapperId === undefined) throw Error ('no parameter.');
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var InfiniteScroll = function () {
+    function InfiniteScroll(path, wrapperId) {
+        _classCallCheck(this, InfiniteScroll);
+
+        if (path === undefined || wrapperId === undefined) throw Error('no parameter.');
         this.path = path;
         this.pNum = 2;
         this.wNode = document.getElementById(wrapperId);
@@ -10,40 +18,54 @@ class InfiniteScroll {
         this.detectScroll();
     }
 
-    detectScroll() {
-        window.onscroll = (ev) => {
-            if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) 
-                this.getNewPost();
-        };    
-    }
-    getNewPost() {
-        if (this.enable === false) return false;
-        this.enable = false;
-        const xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = () => {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200) {
-                    this.pNum++;
-                    const childItems = this.getChildItemsByAjaxHTML(xmlhttp.responseText);
-                    this.appendNewItems(childItems);
-                }
-                return this.enable = true;
-            }
+    _createClass(InfiniteScroll, [{
+        key: 'detectScroll',
+        value: function detectScroll() {
+            var _this = this;
+
+            window.onscroll = function (ev) {
+                if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) _this.getNewPost();
+            };
         }
-        xmlhttp.open("GET", `${location.origin + this.path + this.pNum}/index.html`, true);
-        xmlhttp.send();
-    }
+    }, {
+        key: 'getNewPost',
+        value: function getNewPost() {
+            var _this2 = this;
 
-    getChildItemsByAjaxHTML(HTMLText) {
-        const newHTML = document.createElement('html');
-        newHTML.innerHTML = HTMLText;
-        const childItems = newHTML.querySelectorAll(`#${this.wrapperId} > *`);
-        return childItems;
-    }
+            if (this.enable === false) return false;
+            this.enable = false;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                    if (xmlhttp.status == 200) {
+                        _this2.pNum++;
+                        var childItems = _this2.getChildItemsByAjaxHTML(xmlhttp.responseText);
+                        _this2.appendNewItems(childItems);
+                    }
+                    return _this2.enable = true;
+                }
+            };
+            xmlhttp.open("GET", location.origin + this.path + this.pNum + '/index.html', true);
+            xmlhttp.send();
+        }
+    }, {
+        key: 'getChildItemsByAjaxHTML',
+        value: function getChildItemsByAjaxHTML(HTMLText) {
+            var newHTML = document.createElement('html');
+            newHTML.innerHTML = HTMLText;
+            var childItems = newHTML.querySelectorAll('#' + this.wrapperId + ' > *');
+            return childItems;
+        }
+    }, {
+        key: 'appendNewItems',
+        value: function appendNewItems(items) {
+            var _this3 = this;
 
-    appendNewItems(items) {
-        items.forEach(item => {
-            this.wNode.appendChild(item);
-        });
-    }
-}
+            items.forEach(function (item) {
+                _this3.wNode.appendChild(item);
+            });
+        }
+    }]);
+
+    return InfiniteScroll;
+}();
